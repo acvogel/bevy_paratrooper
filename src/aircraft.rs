099d70ce69bda20ecdab52d477b33a056df9a987
Adam Vogel <acvogel@gmail.com>
2022-02-26 22:35:30 -0800

    Aircraft spawning and clock

--- src/aircraft.rs
+++ src/aircraft.rs
@@ -1,24 +1,40 @@
 use bevy::prelude::*;
 
-// what about a global cooldown resource for how many aircraft?
+use rand::Rng;
 
 #[derive(Component, Default)]
 pub struct Aircraft {
     velocity_x: f32,
 }
 
+//struct Fleet {
+//    aircraft: Vec<Aircraft>,
+//}
+
+const AIRCRAFT_SPEED: f32 = 20.;
+const SPAWN_PROBABILITY: f32 = 0.012;
+
+fn should_spawn_aircraft() -> bool {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0.0..1.0) < SPAWN_PROBABILITY
+}
+
 fn spawn_aircraft(mut commands: Commands, _time: Res<Time>) {
-    commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(1.0, 0., 0.),
-                custom_size: Some(Vec2::new(30., 10.)),
+    if should_spawn_aircraft() {
+        commands
+            .spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(1.0, 0., 0.),
+                    custom_size: Some(Vec2::new(30., 10.)),
+                    ..Default::default()
+                },
+                transform: Transform::from_translation(Vec3::new(0., 100., 5.)),
                 ..Default::default()
-            },
-            transform: Transform::from_translation(Vec3::new(0., 100., 5.)),
-            ..Default::default()
-        })
-        .insert(Aircraft { velocity_x: 20. });
+            })
+            .insert(Aircraft {
+                velocity_x: AIRCRAFT_SPEED,
+            });
+    }
 }
 
 fn fly_aircraft(time: Res<Time>, mut query: Query<(&Aircraft, &mut Transform)>) {
@@ -27,11 +43,13 @@ fn fly_aircraft(time: Res<Time>, mut query: Query<(&Aircraft, &mut Transform)>) 
     }
 }
 
+// detect when they have left the screen, count those in score.
+//fn despawn_aircraft()
+
 pub struct AircraftPlugin;
 
 impl Plugin for AircraftPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_aircraft)
-            .add_system(fly_aircraft);
+        app.add_system(spawn_aircraft).add_system(fly_aircraft);
     }
 }
--- src/score.rs
+++ src/score.rs
@@ -4,6 +4,7 @@ use bevy::prelude::*;
 pub struct Score {
     pub shots: u32,
     pub aircraft_kills: u32,
+    pub aircraft_escapes: u32,
     pub paratrooper_kills: u32,
     //pub start_time: f64, // good to put a clock also?
 }
@@ -16,12 +17,48 @@ fn print_score(score: Res<Score>) {
 }
 
 // UI: simple text somewhere like upper left for now
-fn setup_score_ui(mut commands: Commands) {}
+fn setup_score_ui(mut commands: Commands, time: Res<Time>, asset_server: Res<AssetServer>) {
+    let minutes = (time.seconds_since_startup() / 60.).floor();
+    let seconds = (time.seconds_since_startup() % 60.).floor();
+    let clock_str = format!("{:02}:{:02}", minutes, seconds);
+
+    let font_handle: Handle<Font> = asset_server.load("fonts/FiraSans-Bold.ttf");
+    commands.insert_resource(font_handle);
+
+    // Top center Timer 00:00 MM:SS
+    commands.spawn_bundle(TextBundle {
+        style: Style {
+            align_self: AlignSelf::FlexEnd,
+            position_type: PositionType::Absolute,
+            position: Rect {
+                top: Val::Px(15.),
+                left: Val::Px(15.),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        text: Text::with_section(
+            clock_str,
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 600.,
+                color: Color::ORANGE,
+            },
+            TextAlignment {
+                //horizontal: HorizontalAlign::Center,
+                ..Default::default()
+            },
+        ),
+        ..Default::default()
+    });
+}
 
 pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Score>().add_system(print_score);
+        app.init_resource::<Score>()
+            .add_startup_system(setup_score_ui);
+        //.add_system(print_score);
     }
 }
