8a5ad8f883d8d6451e451aece21126f261cc7154
Adam Vogel <acvogel@gmail.com>
2022-03-27 08:15:21 -0700

    remove dead code

--- src/aircraft.rs
+++ src/aircraft.rs
@@ -83,69 +83,6 @@ fn spawn_aircraft_system_rapier(mut commands: Commands, aircraft_textures: Res<A
     }
 }
 
-//fn spawn_aircraft_system(mut commands: Commands, aircraft_textures: Res<AircraftTextures>) {
-//    if should_spawn_aircraft() {
-//        let aircraft = create_aircraft();
-//
-//        commands
-//            .spawn_bundle(SpriteBundle {
-//                // 412 x 114 pixels. 0.3 scale.
-//                texture: aircraft_textures.image_handle.clone(),
-//                sprite: Sprite {
-//                    //custom_size: Some(Vec2::new(412., 114.)),
-//                    flip_x: aircraft.velocity_x < 0.,
-//                    ..Default::default()
-//                },
-//                transform: aircraft.position.with_scale(Vec3::splat(0.3)),
-//
-//                ..Default::default()
-//            })
-//            .insert(aircraft);
-//    }
-//}
-
-//fn create_aircraft() -> Aircraft {
-//    let mut rng = rand::thread_rng();
-//    let y = rng.gen_range(0. ..350.); // TODO clearance lanes to avoid existing planes, unless same direction
-//    let heading_right = rng.gen_bool(0.5);
-//    let speed = rng.gen_range(0.8..1.3) * AIRCRAFT_SPEED;
-//    if heading_right {
-//        Aircraft {
-//            velocity_x: speed,
-//            position: Transform::from_translation(Vec3::new(SPAWN_LEFT_X, y, 5.)),
-//        }
-//    } else {
-//        Aircraft {
-//            velocity_x: -speed,
-//            position: Transform::from_translation(Vec3::new(SPAWN_RIGHT_X, y, 5.)),
-//        }
-//    }
-//}
-
-//fn fly_aircraft(time: Res<Time>, mut query: Query<(&mut Aircraft, &mut Transform)>) {
-//    for (mut aircraft, mut transform) in query.iter_mut() {
-//        transform.translation.x += aircraft.velocity_x * time.delta_seconds();
-//        aircraft.position.translation.x += aircraft.velocity_x * time.delta_seconds();
-//    }
-//}
-
-///// detect when they have left the screen, count those in score.
-//fn despawn_aircraft(
-//    mut commands: Commands,
-//    mut score: ResMut<Score>,
-//    mut query: Query<(Entity, &Aircraft, &Transform)>,
-//) {
-//    for (e, _aircraft, transform) in query.iter_mut() {
-//        if transform.translation.x >= SPAWN_RIGHT_X + 1.
-//            || transform.translation.x <= SPAWN_LEFT_X - 1.
-//        {
-//            score.aircraft_escapes += 1;
-//            commands.entity(e).despawn();
-//            info!("ESCAPE {}", score.aircraft_escapes);
-//        }
-//    }
-//}
-
 fn setup_aircraft_system(mut commands: Commands, asset_server: ResMut<AssetServer>) {
     commands.insert_resource(AircraftTextures {
         image_handle: asset_server.load("gfx/planes/paraplane1.png"),
@@ -172,9 +109,5 @@ impl Plugin for AircraftPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup_aircraft_system)
             .add_system(spawn_aircraft_system_rapier);
-        //.add_system(aircraft_collision_system);
-        //.add_system(spawn_aircraft_system)
-        //.add_system(despawn_aircraft)
-        //.add_system(fly_aircraft);
     }
 }
