07f32caa8a4874364f3ecf130df2189c59999931
Adam Vogel <acvogel@gmail.com>
2022-04-02 21:53:20 -0700

    Game clock for pausing and reset

--- src/score.rs
+++ src/score.rs
@@ -1,5 +1,6 @@
 use crate::AppState;
 use bevy::prelude::*;
+use std::time::Duration;
 
 use crate::events::*;
 
@@ -18,7 +19,9 @@ pub struct ClockText;
 
 /// AppState::InGame time
 #[derive(Component)]
-pub struct GameTime;
+pub struct GameClock {
+    duration: Duration,
+}
 
 fn kill_listener_system(mut events: EventReader<BulletCollisionEvent>, mut score: ResMut<Score>) {
     for bullet_collision_event in events.iter() {
@@ -51,10 +54,24 @@ fn gun_explosion_listener_system(
     }
 }
 
-fn update_clock(time: Res<Time>, mut query: Query<&mut Text, With<ClockText>>, score: Res<Score>) {
+fn update_clock_ui(
+    game_clock: Res<GameClock>,
+    mut query: Query<&mut Text, With<ClockText>>,
+    score: Res<Score>,
+) {
     for mut text in query.iter_mut() {
-        text.sections[0].value = get_clock_string(time.seconds_since_startup(), *score);
+        text.sections[0].value = get_clock_string(game_clock.duration.as_secs_f64(), *score);
     }
+}
+
+fn setup_game_clock(mut commands: Commands) {
+    commands.insert_resource(GameClock {
+        duration: Duration::ZERO,
+    });
+}
+
+fn update_game_clock(mut game_clock: ResMut<GameClock>, time: Res<Time>) {
+    game_clock.duration += Duration::from_secs_f64(time.delta_seconds_f64());
 }
 
 fn get_clock_string(seconds_since_startup: f64, score: Score) -> String {
@@ -69,7 +86,7 @@ fn get_clock_string(seconds_since_startup: f64, score: Score) -> String {
 }
 
 // UI: simple text somewhere like upper left for now
-fn setup_score_ui(mut commands: Commands, _time: Res<Time>, asset_server: Res<AssetServer>) {
+fn setup_score_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Top center Timer MM:SS
     commands
         .spawn_bundle(TextBundle {
@@ -106,20 +123,31 @@ fn despawn_score_ui(mut commands: Commands, query: Query<Entity, With<ClockText>
     }
 }
 
+fn game_over(mut game_clock: ResMut<GameClock>, mut score: ResMut<Score>) {
+    game_clock.duration = Duration::ZERO;
+    *score = Score::default();
+}
+
 pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<Score>()
-            .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(setup_score_ui))
+            .add_system_set(
+                SystemSet::on_enter(AppState::InGame)
+                    .with_system(setup_game_clock)
+                    .with_system(setup_score_ui),
+            )
             .add_system_set(
                 SystemSet::on_update(AppState::InGame)
                     .with_system(kill_listener_system)
                     .with_system(gun_listener_system)
                     .with_system(landing_listener_system)
-                    .with_system(update_clock)
-                    .with_system(gun_explosion_listener_system),
+                    .with_system(gun_explosion_listener_system)
+                    .with_system(update_game_clock)
+                    .with_system(update_clock_ui),
             )
-            .add_system_set(SystemSet::on_exit(AppState::InGame).with_system(despawn_score_ui));
+            .add_system_set(SystemSet::on_exit(AppState::InGame).with_system(despawn_score_ui))
+            .add_system_set(SystemSet::on_enter(AppState::GameOver).with_system(game_over));
     }
 }
