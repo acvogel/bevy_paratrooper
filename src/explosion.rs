ba872a0751470a9d79e3309d30dc0b9e0062e63f
Adam Vogel <acvogel@gmail.com>
2024-02-27 12:29:04 -0800

    Update explosion TextureAtlas to TextureAtlasLayout

--- src/explosion.rs
+++ src/explosion.rs
@@ -12,9 +12,12 @@ struct AnimationTimer(Timer);
 
 #[derive(Resource)]
 struct ExplosionTextures {
-    air_explosion_texture_atlas_handle: Handle<TextureAtlas>,
-    gib_texture_atlas_handle: Handle<TextureAtlas>,
-    ground_explosion_texture_atlas_handle: Handle<TextureAtlas>,
+    air_explosion_texture_atlas_handle: Handle<TextureAtlasLayout>,
+    air_explosion_texture_handle: Handle<Image>,
+    gib_texture_atlas_handle: Handle<TextureAtlasLayout>,
+    gib_texture_handle: Handle<Image>,
+    ground_explosion_texture_atlas_handle: Handle<TextureAtlasLayout>,
+    ground_explosion_texture_handle: Handle<Image>,
 }
 
 const EXPLOSION_TICK: f32 = 0.1;
@@ -22,22 +25,31 @@ const GIB_TICK: f32 = 0.1;
 
 fn spawn_explosion_system(
     mut commands: Commands,
-    explosion_textures: Res<ExplosionTextures>,
+    explosion_textures: &Res<ExplosionTextures>,
     time: Res<Time>,
     mut event_reader: EventReader<ExplosionEvent>,
 ) {
     for event in event_reader.read() {
-        let explosion_texture_atlas = match event.explosion_type {
-            ExplosionType::Bomb => &explosion_textures.ground_explosion_texture_atlas_handle,
-            ExplosionType::Aircraft | ExplosionType::Bullet => {
-                &explosion_textures.air_explosion_texture_atlas_handle
-            }
+        let (explosion_texture_atlas, explosion_texture) = match event.explosion_type {
+            ExplosionType::Bomb => (
+                *explosion_textures.ground_explosion_texture_atlas_handle,
+                *explosion_textures.ground_explosion_texture_handle,
+            ),
+            ExplosionType::Aircraft | ExplosionType::Bullet => (
+                *explosion_textures.air_explosion_texture_atlas_handle,
+                *explosion_textures.air_explosion_texture_handle,
+            ),
         };
         commands
             .spawn(SpriteSheetBundle {
-                texture_atlas: explosion_texture_atlas.clone(),
+                sprite: Sprite::default(),
+                atlas: TextureAtlas {
+                    layout: explosion_texture_atlas,
+                    index: 0,
+                },
+                texture: explosion_texture,
                 transform: event.transform,
-                ..Default::default()
+                ..default()
             })
             .insert(Explosion(time.elapsed_seconds_f64()))
             .insert(event.explosion_type)
@@ -50,18 +62,20 @@ fn spawn_explosion_system(
 
 fn spawn_gun_explosion_system(
     mut commands: Commands,
-    explosion_textures: Res<ExplosionTextures>,
+    explosion_textures: &Res<ExplosionTextures>,
     time: Res<Time>,
     mut event_reader: EventReader<GunExplosionEvent>,
 ) {
     for event in event_reader.read() {
         commands
             .spawn(SpriteSheetBundle {
-                texture_atlas: explosion_textures
-                    .air_explosion_texture_atlas_handle
-                    .clone(),
+                atlas: TextureAtlas {
+                    layout: *explosion_textures.air_explosion_texture_atlas_handle,
+                    index: 0,
+                },
+                texture: *explosion_textures.air_explosion_texture_handle,
                 transform: Transform::from_translation(event.translation),
-                ..Default::default()
+                ..default()
             })
             .insert(Explosion(time.elapsed_seconds_f64()))
             .insert(AnimationTimer(Timer::from_seconds(
@@ -74,12 +88,12 @@ fn spawn_gun_explosion_system(
 fn animate_explosion_system(
     mut commands: Commands,
     time: Res<Time>,
-    texture_atlases: Res<Assets<TextureAtlas>>,
+    texture_atlases: Res<Assets<TextureAtlasLayout>>,
     mut query: Query<(
         Entity,
         &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-        &Handle<TextureAtlas>,
+        &mut TextureAtlas,
+        &Handle<TextureAtlasLayout>,
     )>,
 ) {
     for (entity, mut timer, mut sprite, animation_texture_atlas_handle) in query.iter_mut() {
@@ -99,57 +113,51 @@ fn animate_explosion_system(
 fn setup_explosion_system(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
 ) {
     // Air Explosion
     let air_explosion_texture_handle = asset_server.load("images/airplaneexplosion.png");
-    let air_explosion_texture_atlas = TextureAtlas::from_grid(
-        air_explosion_texture_handle,
-        Vec2::new(128., 128.),
-        8,
-        1,
-        None,
-        None,
-    );
+    let air_explosion_texture_atlas =
+        TextureAtlasLayout::from_grid(Vec2::new(128., 128.), 8, 1, None, None);
     let air_explosion_texture_atlas_handle = texture_atlases.add(air_explosion_texture_atlas);
 
     // Bomb Explosion
     let ground_explosion_texture_handle = asset_server.load("images/ground_explosion.png");
-    let ground_explosion_textrue_atlas = TextureAtlas::from_grid(
-        ground_explosion_texture_handle,
-        Vec2::new(128., 128.),
-        8,
-        1,
-        None,
-        None,
-    );
+    let ground_explosion_textrue_atlas =
+        TextureAtlasLayout::from_grid(Vec2::new(128., 128.), 8, 1, None, None);
     let ground_explosion_texture_atlas_handle = texture_atlases.add(ground_explosion_textrue_atlas);
 
     // Gib
     let gib_texture_handle = asset_server.load("images/blood1.png");
-    let gib_texture_atlas =
-        TextureAtlas::from_grid(gib_texture_handle, Vec2::new(128., 128.), 8, 1, None, None);
+    let gib_texture_atlas = TextureAtlasLayout::from_grid(Vec2::new(128., 128.), 8, 1, None, None);
     let gib_texture_atlas_handle = texture_atlases.add(gib_texture_atlas);
 
     commands.insert_resource(ExplosionTextures {
         air_explosion_texture_atlas_handle,
+        air_explosion_texture_handle,
         gib_texture_atlas_handle,
+        gib_texture_handle,
         ground_explosion_texture_atlas_handle,
+        ground_explosion_texture_handle,
     });
 }
 
 fn spawn_gib_system(
     mut commands: Commands,
-    explosion_textures: Res<ExplosionTextures>,
+    explosion_textures: &Res<ExplosionTextures>,
     time: Res<Time>,
     mut event_reader: EventReader<GibEvent>,
 ) {
     for event in event_reader.read() {
         commands
             .spawn(SpriteSheetBundle {
-                texture_atlas: explosion_textures.gib_texture_atlas_handle.clone(),
+                atlas: TextureAtlas {
+                    layout: *explosion_textures.gib_texture_atlas_handle,
+                    index: 0,
+                },
+                texture: *explosion_textures.gib_texture_handle,
                 transform: event.transform,
-                ..Default::default()
+                ..default()
             })
             .insert(Gib(time.elapsed_seconds_f64()))
             .insert(AnimationTimer(Timer::from_seconds(
