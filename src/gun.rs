ec94007cd81372c8750b7cdaf9bc5768b5c095c8
Adam Vogel <acvogel@gmail.com>
2026-09-14 15:01:00 -0700

    Update gun shapes and events

--- src/gun.rs
+++ src/gun.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
-use bevy::sprite::Anchor::BottomCenter;
+use bevy::sprite::Anchor;
+use bevy::transform::helper::ComputeGlobalTransformError::MissingTransform;
 use bevy_prototype_lyon::entity::ShapeBundle;
 use bevy_prototype_lyon::prelude::*;
 use bevy_rapier2d::prelude::*;
@@ -38,17 +39,15 @@ pub fn setup_gun_base(mut commands: Commands) {
     let h = GUN_BASE_Y;
     let w = GUN_BASE_X;
     let y = consts::GROUND_Y + 0.5 * h;
-    let sprite_bundle = SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(-0.1, 0.1, 0.1),
-            custom_size: Some(Vec2::new(w, h)),
-            ..default()
-        },
-        transform: Transform::from_translation(Vec3::new(0., y, 2.)),
+    let sprite = Sprite {
+        color: Color::srgb(-0.1, 0.1, 0.1),
+        custom_size: Some(Vec2::new(w, h)),
         ..default()
     };
+    let transform = Transform::from_translation(Vec3::new(0., y, 2.));
     commands
-        .spawn(sprite_bundle)
+        .spawn(sprite)
+        .insert(transform)
         .insert(RigidBody::Fixed)
         .insert(Collider::cuboid(0.5 * w, 0.5 * h))
         .insert(Friction {
@@ -68,6 +67,7 @@ pub fn setup_gun_mount(mut commands: Commands) {
     let mount_rectangle_shape = shapes::Rectangle {
         extents: Vec2::new(GUN_MOUNT_X, GUN_MOUNT_Y),
         origin: RectangleOrigin::Center,
+        ..default()
     };
     let rectangle_y = consts::GROUND_Y + GUN_BASE_Y + 0.5 * GUN_MOUNT_Y;
 
@@ -102,18 +102,16 @@ pub fn setup_gun_mount(mut commands: Commands) {
 pub fn setup_gun_barrel(mut commands: Commands) {
     let y = consts::GROUND_Y + GUN_BASE_Y + GUN_MOUNT_Y; //+ 0.5 * GUN_HEIGHT;
     let sprite_size = Vec2::new(GUN_WIDTH, GUN_HEIGHT);
-    let sprite_bundle = SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.32, 0.36, 0.41),
-            custom_size: Some(sprite_size),
-            anchor: BottomCenter,
-            ..default()
-        },
-        transform: Transform::from_translation(Vec3::new(0., y, 1.)),
+    let sprite = Sprite {
+        color: Color::srgb(0.32, 0.36, 0.41),
+        custom_size: Some(sprite_size),
         ..default()
     };
+    let transform = Transform::from_translation(Vec3::new(0., y, 1.));
     commands
-        .spawn(sprite_bundle)
+        .spawn(sprite)
+        .insert(transform)
+        .insert(Anchor::BOTTOM_CENTER)
         .insert(RigidBody::KinematicVelocityBased)
         .insert(Collider::cuboid(0.5 * sprite_size.x, 0.5 * sprite_size.y))
         .insert(ActiveEvents::COLLISION_EVENTS)
@@ -129,7 +127,6 @@ pub fn setup_gun_barrel(mut commands: Commands) {
 
 /// Move gun with keyboard or gamepad, within bounds.
 fn move_gun(
-    gamepads: Res<Gamepads>,
     button_inputs: Res<ButtonInput<GamepadButton>>,
     keyboard_inputs: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&mut Velocity, &Transform), With<Gun>>,
@@ -137,25 +134,15 @@ fn move_gun(
     // Resolve keyboard and gamepad inputs
     let keyboard_left = keyboard_inputs.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]);
     let keyboard_right = keyboard_inputs.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]);
-    let gamepad_right = gamepads
-        .iter()
-        .find(|&gamepad| {
-            button_inputs.pressed(GamepadButton::new(gamepad, GamepadButtonType::DPadRight))
-        })
-        .is_some();
-    let gamepad_left = gamepads
-        .iter()
-        .find(|&gamepad| {
-            button_inputs.pressed(GamepadButton::new(gamepad, GamepadButtonType::DPadLeft))
-        })
-        .is_some();
+    let gamepad_right = button_inputs.pressed(GamepadButton::DPadRight);
+    let gamepad_left = button_inputs.pressed(GamepadButton::DPadLeft);
     let any_left = keyboard_left || gamepad_left;
     let any_right = keyboard_right || gamepad_right;
 
     // Rotate the gun
-    let (mut velocity, transform) = query.get_single_mut().expect("Gun entity not found!");
+    let (mut velocity, transform) = query.single_mut().expect("Gun entity not found!");
     let (gun_axis, gun_angle) = transform.rotation.to_axis_angle();
-    velocity.angvel = if any_left && any_right {
+    velocity.angular = if any_left && any_right {
         0.
     } else if any_left && (gun_axis.z <= 0. || gun_angle < BOUNDARY_ANGLE) {
         ANGULAR_VELOCITY
@@ -168,28 +155,28 @@ fn move_gun(
 
 /// Bomb colliding with any part of gun assembly causes explosion
 fn gun_bomb_collision_system(
-    mut event_reader: EventReader<CollisionEvent>,
-    mut event_writer: EventWriter<GunExplosionEvent>,
+    mut event_reader: MessageReader<CollisionEvent>,
+    mut event_writer: MessageWriter<GunExplosionEvent>,
     gun_query: Query<(Entity, &Transform), With<Gun>>,
     gun_mount_query: Query<(Entity, &Transform), With<GunMount>>,
     gun_base_query: Query<(Entity, &Transform), With<GunBase>>,
     bombs_query: Query<Entity, With<Bomb>>,
 ) {
-    let (gun_entity, gun_transform) = gun_query.get_single().expect("No gun entity.");
+    let (gun_entity, gun_transform) = gun_query.single().expect("No gun entity.");
     let (gun_mount_entity, gun_mount_transform) =
-        gun_mount_query.get_single().expect("No gun mount entity.");
+        gun_mount_query.single().expect("No gun mount entity.");
     let (gun_base_entity, _gun_base_transform) =
-        gun_base_query.get_single().expect("No gun base entity.");
+        gun_base_query.single().expect("No gun base entity.");
     let targets = HashSet::from([gun_entity, gun_mount_entity, gun_base_entity]);
     for &collision_event in event_reader.read() {
         if let CollisionEvent::Started(entity1, entity2, _flags) = collision_event {
             let is_bomb_collision = bombs_query.contains(entity1) || bombs_query.contains(entity2);
             let is_gun_collision = targets.contains(&entity1) || targets.contains(&entity2);
             if is_bomb_collision && is_gun_collision {
-                event_writer.send(GunExplosionEvent {
+                event_writer.write(GunExplosionEvent {
                     translation: gun_transform.translation,
                 });
-                event_writer.send(GunExplosionEvent {
+                event_writer.write(GunExplosionEvent {
                     translation: gun_mount_transform.translation,
                 });
             }
@@ -199,15 +186,15 @@ fn gun_bomb_collision_system(
 
 /// Landed paratrooper colliding with gun or mount causes explosion
 fn gun_paratrooper_collision_system(
-    mut event_reader: EventReader<CollisionEvent>,
-    mut event_writer: EventWriter<GunExplosionEvent>,
+    mut event_reader: MessageReader<CollisionEvent>,
+    mut event_writer: MessageWriter<GunExplosionEvent>,
     gun_query: Query<(Entity, &Transform), With<Gun>>,
     gun_mount_query: Query<(Entity, &Transform), With<GunMount>>,
     paratroopers_query: Query<Entity, With<Paratrooper>>,
 ) {
-    let (gun_entity, gun_transform) = gun_query.get_single().expect("No gun entity.");
+    let (gun_entity, gun_transform) = gun_query.single().expect("No gun entity.");
     let (gun_mount_entity, gun_mount_transform) =
-        gun_mount_query.get_single().expect("No gun mount entity.");
+        gun_mount_query.single().expect("No gun mount entity.");
     let targets = HashSet::from([gun_entity, gun_mount_entity]);
     for &collision_event in event_reader.read() {
         if let CollisionEvent::Started(entity1, entity2, _flags) = collision_event {
@@ -215,10 +202,10 @@ fn gun_paratrooper_collision_system(
                 paratroopers_query.contains(entity1) || paratroopers_query.contains(entity2);
             let is_gun_collision = targets.contains(&entity1) || targets.contains(&entity2);
             if is_bomb_collision && is_gun_collision {
-                event_writer.send(GunExplosionEvent {
+                event_writer.write(GunExplosionEvent {
                     translation: gun_transform.translation,
                 });
-                event_writer.send(GunExplosionEvent {
+                event_writer.write(GunExplosionEvent {
                     translation: gun_mount_transform.translation,
                 });
             }
@@ -228,8 +215,8 @@ fn gun_paratrooper_collision_system(
 
 /// Stop gun rotation.
 fn stop_gun(mut gun_query: Query<&mut Velocity, With<Gun>>) {
-    let mut velocity = gun_query.get_single_mut().expect("Gun velocity not found.");
-    velocity.angvel = 0.;
+    let mut velocity = gun_query.single_mut().expect("Gun velocity not found.");
+    velocity.angular = 0.;
 }
 
 pub struct GunPlugin;
